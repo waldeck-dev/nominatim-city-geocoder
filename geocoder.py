14a39b2541b29256f8171bb76382760e93b72956
Valentin <valentin@waldeck.dev>
2024-02-18 10:53:58 +0100

    feat: make sure names are always URL-safe
    Signed-off-by: Valentin <valentin@waldeck.dev>

--- geocoder.py
+++ geocoder.py
@@ -1,5 +1,6 @@
 import json
 from urllib.request import urlopen
+from urllib.parse import quote
 
 
 class Geocoder:
@@ -8,7 +9,10 @@ class Geocoder:
     FORMAT = "jsonv2"
 
     def get_query(self, city, country):
-        return f"city={city}&country={country}&format={self.FORMAT}&limit=1"
+        return (
+            f"city={quote(city)}&country={quote(country)}"
+            f"&format={self.FORMAT}&limit=1"
+        )
 
     def geocode(self, city, country):
         url = f"{self.BASE_URL}?{self.get_query(city, country)}"
